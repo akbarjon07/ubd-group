f7a76278294103c4b2c77431239a165f4acc8292
akbarjon07 <akbarjon.1019@gmail.com>
2023-02-12 15:17:25 +0500

    sixth commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,15 @@
 import './header.css'
-import {Link, NavLink} from 'react-router-dom'
+import { useRef } from 'react'
+import { Link, NavLink } from 'react-router-dom'
 import logo from '../../assets/images/site-logo.svg'
 
 export const Header = () => {
+
+    const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle("responsive_nav");
+	};
 
     return (
         <header className='header'>
@@ -11,7 +18,7 @@ export const Header = () => {
                     <img className='header__logo-img' src={logo} alt="site-logo" width={202} height={40} />
                 </Link>
 
-                <nav className='header__nav'>
+                <nav ref={navRef} className='header__nav'>
                     <ul className='header__nav-list'>
                         <li className='header__nav-item'>
                             <NavLink className='header__nav-link' to="#">главная</NavLink>
@@ -29,11 +36,17 @@ export const Header = () => {
                             <NavLink className='header__nav-link' to="#">контакты</NavLink>
                         </li>
                     </ul>
+
+                    <button onClick={showNavbar} className='header__close-btn nav-btn'><i className="bi bi-x-lg"></i></button>
                 </nav>
 
                 <button className="header__btn">
                     <i className="bi bi-plus-lg"></i>
                     Оставить заявку
+                </button>
+
+                <button className='header__menu-btn nav-btn'>
+                    <i onClick={showNavbar} className="bi bi-list"></i>
                 </button>
             </div>
         </header>
