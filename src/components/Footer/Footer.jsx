431eb6f1d1401a2ba1ce3664b6fb5411927f5611
akbarjon07 <akbarjon.1019@gmail.com>
2023-02-12 11:10:13 +0500

    fourth commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -22,15 +22,15 @@ export const Footer = () => {
                     </li>
 
                     <li className='footer__nav-item'>
-                        <NavLink className='footer__nav-link' to='#'>Продукция</NavLink>
+                        <NavLink className='footer__nav-link res-link' to='#'>Продукция</NavLink>
 
-                        <span className='footer__nav-span'></span>
+                        <span className='footer__nav-span res-span'></span>
                     </li>
 
                     <li className='footer__nav-item'>
                         <NavLink className='footer__nav-link' to='#'>Контакты</NavLink>
 
-                        <span className='footer__nav-span res-span'></span>
+                        <span className='footer__nav-span'></span>
                     </li>
 
                     <li className='footer__nav-item'>
